ec37ad932a7472b92cc4b4e8652c6802a7062019
woxane <77023555+woxane@users.noreply.github.com>
2021-08-20 01:00:12 +0430

    Update changer.py

--- subtitle-changer/changer.py
+++ subtitle-changer/changer.py
@@ -3,10 +3,10 @@
 import subprocess
 import os 
 
-a = subprocess.check_output(["ls"]).decode().splitlines()
+files = subprocess.check_output(["ls"]).decode().splitlines()
 
-mkv = [i[:-4] for i in a if i[-3:] == "mkv"]
-srt = [i[:-4] for i in a if i[-3:] == "srt"]
+mkv = [i[:-4] for i in files if i[-3:] == "mkv"]
+srt = [i[:-4] for i in files if i[-3:] == "srt"]
 
 for i in range(0,len(mkv)) :
     print(srt[i])
